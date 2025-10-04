700ae5f63fda4eda74975b2200f8a3ca7ab2e809
516 Hackers <125684516+516hackers@users.noreply.github.com>
2025-10-04 22:41:52 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read().splitlines()
 
 setup(
-    name="516-osint-toolkit",
+    name="516-digital-investigation-tools",
     version="1.0.0",
     author="516 Hackers",
     author_email="your-email@example.com",
-    description="A comprehensive OSINT and media analysis toolkit",
+    description="A comprehensive digital investigation and OSINT toolkit by 516 Hackers",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
@@ -29,11 +29,14 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'sherlock516=scripts.sherlock_wrapper:main',
+            'investigate516=scripts.sherlock_wrapper:main',
             'ig516=scripts.enhanced_instaloader:main',
             'meta516=scripts.metadata_analyzer:main',
-            'img516=scripts.image_forensics:main',
+            'image516=scripts.image_forensics:main',
             'social516=scripts.social_media_mapper:main',
+            'email516=scripts.email_analyzer:main',
+            'domain516=scripts.domain_research:main',
+            'report516=scripts.report_generator:main',
         ],
     },
 )
